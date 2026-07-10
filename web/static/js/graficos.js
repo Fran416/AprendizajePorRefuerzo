89a30da2c9ddd56253a20b5da9a0328ee5307418
fran416 <lizama.osorio416@gmail.com>
2026-07-10 12:03:31 -0400

    refactor: Eliminar codigo muerto y actualizar conceptos.html - Se elmino graficos.js, considerado en un inicio y eliminado por no uso en la version final

--- web/static/js/graficos.js
+++
@@ -1,3 +0,0 @@
-// Funciones para generar y actualizar gráficos interactivos con Plotly.js:
-// heatmap de tabla Q, curva de recompensas, indicadores de sensores y
-// timeline de detecciones de anomalías.
